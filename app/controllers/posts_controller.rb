901355da94acef3227990f55fd490b22cee70167
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2024-03-14 20:51:32 +1030

    Add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,24 @@
 class PostsController < ApplicationController
-  def new
+  def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to action: "index"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def index
-
-  end
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
